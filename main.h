a27f50feac55be2394963bb156051fe66dc753ab
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-13 13:58:18 +0000

    Task 0

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
 
-int print_c(va_list args);
-#endif 
+int _printf(const char *format, ...);
+int print_char(va_list args);
+
+#endif
