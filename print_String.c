4c176911b31db7a6dc2d8030d5927870bbd0f470
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-16 14:37:17 +0000

    TaskS

---
+++ print_String.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * print_string - prints a string
+ * @s: string to print
+ * Return: number of printed characters
+ */
+
+int print_string(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+
+	return (i);
+}i
